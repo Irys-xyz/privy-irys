13df09a340c31dccd691a6e672a338bf7a4df5be
Luke Cassady-Dorion <luke@SPStories.com>
2024-01-23 17:27:29 -0300

    change nature category to travel

--- components/category-context.tsx
+++ components/category-context.tsx
@@ -2,46 +2,43 @@
 import React, { ReactNode, createContext, useContext, useState } from "react";
 
 export enum CATEGORIES {
-  ALL = "All",
-  PEOPLE = "People",
-  NATURE = "Nature",
-  ANIMALS = "Animals",
-  FOOD = "Food",
+	ALL = "All",
+	PEOPLE = "People",
+	//   NATURE = "Nature",
+	TRAVEL = "Travel",
+	ANIMALS = "Animals",
+	FOOD = "Food",
 }
 
 type CategoryContextValue = {
-  category: CATEGORIES | null;
-  setCategory: (category: CATEGORIES) => void;
-  setShouldUpdate: (p: number) => void;
-  shouldUpdate: number;
+	category: CATEGORIES | null;
+	setCategory: (category: CATEGORIES) => void;
+	setShouldUpdate: (p: number) => void;
+	shouldUpdate: number;
 };
 
-const CategoryContext = createContext<CategoryContextValue | undefined>(
-  undefined
-);
+const CategoryContext = createContext<CategoryContextValue | undefined>(undefined);
 
 type CategoryProviderProps = {
-  children: ReactNode;
+	children: ReactNode;
 };
 
 export const CategoryProvider = ({ children }: CategoryProviderProps) => {
-  const [category, setCategory] = useState<CATEGORIES | null>(null);
-  const [shouldUpdate, setShouldUpdate] = useState<number>(0);
+	const [category, setCategory] = useState<CATEGORIES | null>(null);
+	const [shouldUpdate, setShouldUpdate] = useState<number>(0);
 
-  return (
-    <CategoryContext.Provider
-      value={{ category, setCategory, setShouldUpdate, shouldUpdate }}
-    >
-      {children}
-    </CategoryContext.Provider>
-  );
+	return (
+		<CategoryContext.Provider value={{ category, setCategory, setShouldUpdate, shouldUpdate }}>
+			{children}
+		</CategoryContext.Provider>
+	);
 };
 
 // 5. Create a custom hook for consuming the context
 export const useCategory = () => {
-  const context = useContext(CategoryContext);
-  if (!context) {
-    throw new Error("useCategory must be used within a CategoryProvider");
-  }
-  return context;
+	const context = useContext(CategoryContext);
+	if (!context) {
+		throw new Error("useCategory must be used within a CategoryProvider");
+	}
+	return context;
 };
